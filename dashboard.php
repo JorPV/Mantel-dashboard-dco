8403f8c0eb62d9d32ba0f052950836f7d680d435
JordiPV <88150345+JorPV@users.noreply.github.com>
2023-06-21 14:36:44 +0200

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <? require_once(dirname(__DIR__)."/example/example.php"); ?>
+  <head>
+    <title>Mantel Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700" rel="stylesheet">
+    <link href="./dashboard.css" rel="stylesheet">
+  </head>
+  <body class="bg-primary-80">
+    <div class="js-wrapper">
+      <?=$dashboard_body?>
+    </div>
+    <script type="module" src="js/.../dashboard.js?>"></script>
+  </body>
+</html>
